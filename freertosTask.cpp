ca31e786451d9e8e0f7cde48a08c9afd1644533a
bioresestance <36652096+bioresestance@users.noreply.github.com>
2020-08-31 22:01:47 -0700

    Adding FreeRTOS Task wrapper

---
+++ freertosTask.cpp
@@ -0,0 +1,67 @@
+
+
+#include <FreeRTOSWrappers/freertosTask.h>
+#include <esp_log.h>
+
+
+namespace freeRTOS
+{ 
+
+    Task::Task(string taskName, uint32_t taskStackSize, uint32_t taskPriority)
+    : _taskStackSize(taskStackSize),_taskPriority(taskPriority), _taskName(taskName),_taskHandle(NULL)
+    {
+        xTaskCreatePinnedToCore(taskHandler, taskName.c_str(), taskStackSize, this, taskPriority, &_taskHandle, 1);
+    }
+    
+    Task::Task(string taskName)
+    : Task(taskName, defaultStackSize, defaultPriority) // Calls base ctor with default stack size and priority.
+    {        
+    }
+    
+    Task::~Task()
+    {
+#if INCLUDE_vTaskDelete
+        // If the task was created, we need to Destroy it when object is deleted.
+        if(this->_taskHandle != NULL) {
+            vTaskDelete(this->_taskHandle);
+        }
+#endif
+    }
+
+    void Task::taskHandler(void *_params)
+    {
+        Task *p = static_cast<Task *>(_params);
+        p->taskMain();
+
+        // Should only get here if task ends. Run destructor.
+        p->~Task();
+    }
+
+    string Task::getTaskName()
+    {
+        return _taskName;
+    }
+    
+    uint32_t Task::getTaskPriority()
+    {
+        return _taskPriority;
+    }
+    
+    uint32_t Task::getTaskStackSize()
+    {
+        return _taskStackSize;
+    }
+
+#if INCLUDE_vTaskSuspend  
+    bool Task::suspend() 
+    {
+        vTaskSuspend(this->_taskHandle);
+    }
+    
+    bool Task::resume() 
+    {
+        vTaskResume(this->_taskHandle);
+    }
+#endif
+
+} // namespace freeRTOS
---
+++ freertosTask.h
@@ -0,0 +1,139 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+using std::string;
+
+namespace freeRTOS
+{
+    class Task
+    {
+
+    protected:
+        /****************************************************************************************************************/
+        /*                                           Task Constants                                                     */
+        /****************************************************************************************************************/
+
+        //! Default Task Stack size when there is not one provided.
+        static const uint32_t defaultStackSize = 2048;
+
+        //! DEfault Tasks Priority when there is not one provided.
+        static const uint32_t defaultPriority = 1;
+
+        /****************************************************************************************************************/
+        /*                                           Task Common Variables                                              */
+        /****************************************************************************************************************/
+
+        //! Holds the stack size for the task.
+        uint32_t _taskStackSize;
+
+        //! Holds the priority level for the task.
+        uint32_t _taskPriority;
+
+        //! Holds the name of the task.
+        string _taskName;
+
+        //! Holds the Task Handle for the task.
+        TaskHandle_t _taskHandle;
+
+        /****************************************************************************************************************/
+        /*                                           Task Constructors                                                  */
+        /****************************************************************************************************************/
+    public:
+        /**
+         * @brief Construct a new Task object with all needed values
+         * 
+         * @param taskName Name of the FreeRTOS task.
+         * @param taskStackSize Stack Size to allocate for the task.
+         * @param taskPriority Priority level for the task. 
+         */
+        Task(string taskName, uint32_t taskStackSize, uint32_t taskPriority);
+
+        /**
+         * @brief Construct a new Task object with only a Name. Uses Default priority and stack size.
+         * 
+         * @param taskName Name of the FreeRTOS task.
+         */
+        Task(string taskName);
+
+        /****************************************************************************************************************/
+        /*                                           Task destructors                                                   */
+        /****************************************************************************************************************/
+
+        /**
+         * @brief Destroy the Task object. Deletes the FreeRtos Task.
+         */
+        ~Task();
+
+        /****************************************************************************************************************/
+        /*                                           Task Main Methods                                                  */
+        /****************************************************************************************************************/
+
+        /**
+         * @brief Static function that all FreeRTOS tasks call to start task. Calls overriden taskMain Function/ 
+         * 
+         *  This is the main task function that all FreeRTOS Objects end up first calling. This functions gets called indirectly 
+         *  when a new task is created. The param pointer input contains the "this" pointer. That is used to call the overriden
+         *  taskMain function using dynamic Polymorphism.
+         * 
+         *  \note This function should never be called except as a function pointer to xTaskCreate function.
+         * 
+         * @param _params Parameters of the FreeRTOS task. In reality contains pointer to 'this' Object.
+         */
+        static void taskHandler(void *_params);
+
+        /****************************************************************************************************************/
+        /*                                           Task Public Methods                                                */
+        /****************************************************************************************************************/
+
+        /**
+         * @brief Main function of the FreeRTOS task. 
+         * 
+         */
+        virtual void taskMain() = 0;
+
+        /**
+         * @brief Gets the Task Name.
+         * 
+         * @return string The name of the task.
+         */
+        string getTaskName();
+
+        /**
+         * @brief Get the Task Priority.
+         * 
+         * @return uint32_t The priority level of the task.
+         */
+        uint32_t getTaskPriority();
+
+        /**
+         * @brief Gets the Task Stack Size.
+         * 
+         * @return uint32_t The Stack size of the task.
+         */
+        uint32_t getTaskStackSize();
+
+#if INCLUDE_vTaskSuspend  
+        /**
+         * @brief Suspends the task.
+         * 
+         * @return true Suspend successful
+         * @return false Suspend failed
+         */
+        bool suspend();
+
+        /**
+         * @brief Resumes a suspended task.
+         * 
+         * @return true 
+         * @return false 
+         */
+        bool resume();
+
+#endif
+    };
+
+} // namespace freeRTOS
